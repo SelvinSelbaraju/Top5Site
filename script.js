7b85b0dab8c4efb1dd8913927f92a03141db2269
SelvinSelbaraju <selvinoselbaraju@gmail.com>
2021-02-18 17:48:59 +0000

    update carousel

--- script.js
+++ script.js
@@ -19,7 +19,25 @@ const topEntries = [
         subtext: 'Excite your tastebuds with our Top 5 Drinks',
         buttonText: 'Our Top 5 Drinks',
         id: 'drink'
-    }
+    },
+    {
+        slogan: 'Bored of Trash TV?',
+        subtext: 'Have a laugh or cry with one of these shows',
+        buttonText: 'Our Top 5 TV Shows',
+        id: 'tv'
+    },
+    {
+        slogan: 'Need something other than Romcoms?',
+        subtext: 'Witness the best cinema has to offer',
+        buttonText: 'Our Top 5 Movies',
+        id: 'movie'
+    },
+    {
+        slogan: 'Experience art like never before',
+        subtext: 'Understand why Anime and Manga are so big in Japan',
+        buttonText: 'Our Top 5 Anime/Manga',
+        id: 'animemanga'
+    },
 ]
 
 let entryIndex = 1;
